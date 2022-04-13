3faec62bfc7a8eece57a2da304c7852b2e351718
stnkl <stnkl@users.noreply.github.com>
2022-04-13 16:53:55 +0200

    Default slider icon

--- src/Slider.h
+++ src/Slider.h
@@ -3,24 +3,20 @@
 class Slider
 {
 public:
-    const String name;
-    const int16_t min;
-    const int16_t max;
+    String name;
+    int16_t min;
+    int16_t max;
     int16_t value;
-    const int16_t step;
-    const String icon;
+    int16_t step;
+    String icon;
 
-    Slider(String _name,
-           int16_t _min,
-           int16_t _max,
-           int16_t _default_value,
-           int16_t _step,
-           String _icon) : name(_name),
-                           min(_min),
-                           max(_max),
-                           value(_default_value),
-                           step(_step),
-                           icon(_icon)
+    Slider(String _name, int16_t _min, int16_t _max, int16_t _default_value, int16_t _step, String _icon = "sliders")
     {
+        name = _name;
+        min = _min;
+        max = _max;
+        value = _default_value;
+        step = _step;
+        icon = _icon;
     }
 };
